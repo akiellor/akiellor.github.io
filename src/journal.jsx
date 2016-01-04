08d3403a5572337039c82ced846aae4ce47838bb
Andrew Kiellor <akiellor@gmail.com>
2016-01-04 00:21:28 -0800

    Use react-routing and redux.

--- src/journal.jsx
+++ src/journal.jsx
@@ -1,20 +1,15 @@
 import React from 'react';
-import $ from 'jquery';
+import { connect } from 'react-redux';
 import Header from './header.jsx';
 import Posts from './posts.jsx';
 
-export default React.createClass({
-  getInitialState: function() {
-    return {
-      posts: [],
-      allowDrafts: false
-    }
-  },
-  render: function(){
-    const posts = this.state.posts
-      .filter((post) => {
-        return this.state.allowDrafts || !post.draft;
-      });
-    return <div className="container"><Header /><Posts posts={posts}/></div>
-  }
-})
+function Journal({ posts, allowDrafts }) {
+  posts = posts.filter((post) => {
+    return allowDrafts || !post.draft;
+  });
+  return <div className="container"><Header /><Posts posts={posts}/></div>
+}
+
+export default connect(state => {
+  return {posts: state.posts};
+}, {})(Journal);
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,47 @@ require('highlight.js/styles/github.css');
 require('./main.scss');
 
 import $ from 'jquery';
+import Journal from './journal.jsx';
+import { install } from './asciimation.js';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Journal from './journal.jsx';
-import {install} from './asciimation.js';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import { IndexRoute, Router, Route } from 'react-router';
+import createHistory from 'history/lib/createHashHistory';
+import { syncReduxAndRouter, routeReducer, pushPath } from 'redux-simple-router';
+
+function postsReducer(state = [], action) {
+  if (action.type === 'INITIALISE') {
+    state = action.posts;
+  }
+  return state;
+};
+
+const reducer = combineReducers(Object.assign({}, {
+  routing: routeReducer,
+  posts: postsReducer
+}));
+const store = createStore(reducer);
+const history = createHistory();
+
+syncReduxAndRouter(history, store);
+
+const DraftJournal = React.createClass({
+  render: function() {
+    return <Journal allowDrafts={true} />
+  }
+});
+
+const App = React.createClass({
+  render: function() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+});
 
 function getPosts() {
   return $.get('./model.json').then((model) => {
@@ -26,19 +63,18 @@ function getPosts() {
 
 $(document).ready(function() {
   install();
-  const root = ReactDOM.render(<Journal />, document.getElementById('content'));
+  ReactDOM.render((
+    <Provider store={store}>
+      <Router history={history}>
+        <Route path="/" component={App}>
+          <IndexRoute component={Journal} />
+          <Route path="drafts" component={DraftJournal} />
+        </Route>
+      </Router>
+    </Provider>
+  ), document.getElementById('content'));
+
   getPosts().then((posts) => {
-    root.setState(function(previousState) {
-      previousState.posts = posts;
-      return previousState;
-    });
+    store.dispatch({type: 'INITIALISE', posts: posts});
   });
-  function updateAllowDrafts() {
-    root.setState(function(previousState) {
-      previousState.allowDrafts = window.location.hash === '#drafts';
-      return previousState;
-    });
-  }
-  updateAllowDrafts();
-  setInterval(updateAllowDrafts, 50);
 });
