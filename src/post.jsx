92378f5ae4be90fed6ad4e41db4406660556a035
Andrew Kiellor <akiellor@gmail.com>
2016-01-06 15:52:11 -0800

    Fix linking straight to posts.

--- src/post.jsx
+++ src/post.jsx
@@ -2,7 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { asciimate } from './asciimation';
+import { pushPath } from 'redux-simple-router';
 import $ from 'jquery';
+
+function findPost(posts, id) {
+  return posts.filter((post) => post.id === id)[0];
+}
 
 const Post = React.createClass({
   componentDidUpdate: function() {
@@ -11,9 +16,21 @@ const Post = React.createClass({
       asciimate(e);
     });
   },
+  componentWillUpdate: function() {
+    const { posts, params, pushPath } = this.props;
+
+    const post = findPost(posts, params.id);
+    if (posts.length > 0 && !post) {
+      pushPath('/');
+    }
+  },
   render: function() {
     const { posts, params } = this.props;
-    const post = posts.filter((post) => post.id === params.id)[0];
+    const post = findPost(posts, params.id);
+    if (!post) {
+      return <div />
+    }
+
     var html = { __html: post.content };
     return (
       <div>
@@ -26,4 +43,4 @@ const Post = React.createClass({
 
 export default connect(state => {
   return {posts: state.posts};
-}, {})(Post);
+}, { pushPath })(Post);
