b4d267b143280d13346ca1685d0633cbb62388c1
Andrew Kiellor <akiellor@gmail.com>
2016-01-06 11:02:46 -0800

    Style the post listing better.

--- src/posts.jsx
+++ src/posts.jsx
@@ -1,21 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
-import Post from './post.jsx';
 
 export default React.createClass({
   render: function(){
     const posts = this.props.posts.map(function(post) {
       return (
         <div key={post.id}>
-          <Link to={`post/${post.id}`} params={{ id: post.id }}><h3>{post.title}</h3></Link>
+          <h1 className="title"><Link to={`post/${post.id}`} params={{ id: post.id }}>{post.title}</Link></h1>
           <p>{post.synopsis}</p>
         </div>
       )
     });
     return (
-      <div>
+      <section className="posts">
         {posts}
-      </div>
+      </section>
     )
   }
 })
