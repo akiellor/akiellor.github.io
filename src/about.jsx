37a6dfbd677bd70b84c1d71e7258598d6637474d
Andrew Kiellor <akiellor@gmail.com>
2016-01-06 20:15:28 -0800

    Fix thoughtworks.com link.

--- src/about.jsx
+++ src/about.jsx
@@ -4,7 +4,7 @@ export default React.createClass({
   render: function(){
     return (
       <section className="about">
-        <p>I am Software Developer currently working for <a href="thoughtworks.com">ThoughtWorks</a>.</p>
+        <p>I am Software Developer currently working for <a href="https://thoughtworks.com">ThoughtWorks</a>.</p>
 
         <div className="social">
           <a href="https://github.com/akiellor" className="github">Github</a>
